e5fed41c8dc34442c3351ff3c96336af9c3f81bf
Alexandre de Souza <alexandre@aledsz.com.br>
2022-06-03 19:32:22 -0300

    Add decimal dependency

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule ReqAthena.MixProject do
     [
       {:req, github: "wojtekmach/req"},
       {:aws_signature, "~> 0.3.0"},
+      {:decimal, "~> 2.0.0"},
       {:table, "~> 0.1.1", optional: true}
     ]
   end
