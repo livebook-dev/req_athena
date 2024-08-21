42f1f619de66cab90577ec97818eb8bb6100f57a
Philip Sampaio <philip.sampaio@gmail.com>
2024-08-21 16:46:27 -0300

    Introduce "UNLOAD" command to the query builder
    The "UNLOAD" command allows us to specify the format of the output, so
    we can tell Athena to save our results as Parquet.
    This will be useful for loading them with Explorer.

--- lib/req_athena/query.ex
+++ lib/req_athena/query.ex
@@ -2,7 +2,7 @@ defmodule ReqAthena.Query do
   @moduledoc false
   # This module represents a query and its attributes.
 
-  defstruct query: nil, params: nil, statement_name: nil, prepared: false
+  defstruct query: nil, params: nil, statement_name: nil, prepared: false, unload: nil
 
   @doc """
   Returns if this query is using params or not.
@@ -30,10 +30,10 @@ defmodule ReqAthena.Query do
         if is_nil(query.statement_name),
           do: raise(":statement_name is required for a parameterized query")
 
-        "PREPARE #{query.statement_name} FROM #{query.query}"
+        "PREPARE #{query.statement_name} FROM #{maybe_around_unload(query)}"
 
       true ->
-        query.query
+        maybe_around_unload(query)
     end
   end
 
@@ -54,4 +54,62 @@ defmodule ReqAthena.Query do
   end
 
   defp encode_value(value), do: value
+
+  defp maybe_around_unload(%{query: query_string, unload: [_ | _] = opts})
+       when is_binary(query_string) do
+    # UNLOAD works only with SELECT
+    if query_string =~ ~r/^[\s]*select/i do
+      {to, props} = Keyword.pop!(opts, :to)
+
+      props =
+        Enum.intersperse(
+          for(
+            {key, value} <- props,
+            not is_nil(value),
+            do: [Atom.to_string(key), " = ", encode_value(value)]
+          ),
+          ", "
+        )
+
+      IO.iodata_to_binary([
+        "UNLOAD (",
+        query_string,
+        ")",
+        "\n",
+        "TO ",
+        encode_value(to),
+        "\n",
+        "WITH (",
+        props,
+        ")"
+      ])
+    else
+      query_string
+    end
+  end
+
+  defp maybe_around_unload(%{query: query_string}), do: query_string
+
+  @doc """
+  Add attributes required by the "UNLOAD" command.
+
+  See: https://docs.aws.amazon.com/athena/latest/ug/unload.html
+  """
+  def with_unload(%__MODULE__{} = query, opts) do
+    opts =
+      Keyword.validate!(opts,
+        to: nil,
+        format: "PARQUET",
+        compression: "SNAPPY",
+        compression_level: nil,
+        field_delimiter: nil,
+        partitioned_by: nil
+      )
+
+    if opts[:to] in ["", nil] do
+      raise "`:to` is required by UNLOAD"
+    end
+
+    %{query | unload: opts}
+  end
 end
--- test/req_athena/query_test.exs
+++ test/req_athena/query_test.exs
@@ -33,4 +33,41 @@ defmodule ReqAthena.QueryTest do
                "EXECUTE test_statement USING 420"
     end
   end
+
+  describe "with_unload/2" do
+    test "unload attributes" do
+      query = %Query{query: "SELECT name, id FROM users"}
+      query = Query.with_unload(query, to: "s3://my-bucket/my-dir")
+
+      assert Query.to_query_string(query) ==
+               "UNLOAD (SELECT name, id FROM users)\nTO 's3://my-bucket/my-dir'\nWITH (compression = 'SNAPPY', format = 'PARQUET')"
+    end
+
+    test "unload attributes and a prepare statement does use unload command" do
+      query = %Query{
+        query: "SELECT name, id FROM users WHERE id > ?",
+        params: [420],
+        statement_name: "test_statement"
+      }
+
+      query = Query.with_unload(query, to: "s3://my-bucket/my-dir")
+
+      assert Query.to_query_string(query) ==
+               "PREPARE test_statement FROM UNLOAD (SELECT name, id FROM users WHERE id > ?)\nTO 's3://my-bucket/my-dir'\nWITH (compression = 'SNAPPY', format = 'PARQUET')"
+    end
+
+    test "unload attributes and an execute command does not use the unload command" do
+      query = %Query{
+        query: "SELECT name, id FROM users WHERE id > ?",
+        params: [420],
+        prepared: true,
+        statement_name: "test_statement"
+      }
+
+      query = Query.with_unload(query, to: "s3://my-bucket/my-dir")
+
+      assert Query.to_query_string(query) ==
+               "EXECUTE test_statement USING 420"
+    end
+  end
 end
